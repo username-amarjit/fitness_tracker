341508d2ed4269da56915cc41c5b8d83c062cf7b
Your Name <you@example.com>
2024-09-24 23:04:39 +0530

    forgot password api added

--- fitApp/views.py
+++ fitApp/views.py
@@ -14,3 +14,28 @@ def sign_up(request):
             user = serializer.save()
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def forgot_password(request):
+    if request.method == 'POST':
+        print(request.data)
+        req_username = request.data.get("username") 
+        req_email = request.data.get("email")
+
+        if req_username:
+            user_obj = User.objects.filter(username = req_username,is_active=True)
+        elif req_email:
+            user_obj = User.objects.filter(email = req_email,is_active=True)
+
+        print("4567890",user_obj)
+        if user_obj:
+            email = user_obj.first().email
+            print('email--------',email)
+            if email:
+                # send_email(email)
+                pass
+                return Response({"message":"OTP sent to mail Successfully."},status=status.HTTP_200_OK)
+            return Response({"message":"No mail is set with given username. Kindly contact administrator."},status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"User not found for given email/username."},status=status.HTTP_401_UNAUTHORIZED)
